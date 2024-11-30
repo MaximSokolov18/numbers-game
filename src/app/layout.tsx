72bb2f3ed217a39a813e5118813ab19738219cc6
Maksym Sokolov <maksym.sokolov@pitchbook.com>
2024-11-30 11:35:13 +0100

    rewrite robots

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,19 @@ import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-    robots: { index: false, follow: false },
+    robots: {
+        index: false,
+        follow: true,
+        nocache: true,
+        googleBot: {
+            index: true,
+            follow: false,
+            noimageindex: true,
+            'max-video-preview': -1,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        },
+    },
     title: 'Numbers Game',
     description: 'It`s simple numbers game.',
     other: {
