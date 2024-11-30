335476910fee4acde22b5f5cd2800638b49c635e
Maksym Sokolov <maksym.sokolov@pitchbook.com>
2024-11-30 11:23:01 +0100

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,12 @@ import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
+    robots: { index: false, follow: false },
     title: 'Numbers Game',
-    description: 'It`s simple numbers game.'
+    description: 'It`s simple numbers game.',
+    other: {
+        'google-site-verification': '7Qxh9LoHKMdoTTs2DEvMAjrzpehzFEZN4U4YFtk9Djk'
+    }
 };
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import Head from 'next/head'
-
 export default function Home() {
     return (
-        <>
-            <Head>
-                <meta name="google-site-verification" content="7Qxh9LoHKMdoTTs2DEvMAjrzpehzFEZN4U4YFtk9Djk"/>
-            </Head>
-            <main>
-                <h2>NUMBER GAME - START</h2>
-            </main>
-        </>
-    );
+        <main>
+            <h2>NUMBER GAME - START</h2>
+        </main>
+    )
 }
