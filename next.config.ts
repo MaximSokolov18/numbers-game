03e0c182321715b098cf7648bf22584529837c2e
Maksym Sokolov <maksym.sokolov@pitchbook.com>
2024-12-10 02:51:25 +0100

    add webpack configuration to fix the error with Sharp when deploying on Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    webpack: (config) => ({
+        ...config,
+        externals: [
+            ...config.externals,
+            {
+                sharp: 'commonjs sharp'
+            }
+        ]
+    })
 };
 
 export default nextConfig;
